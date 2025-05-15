76190d9618445b7cf0b075f324c56750d7f2da45
Ashutosh Dwivedi <ashutoshmzp1221@gmail.com>
2025-05-15 12:01:09 +0530

    Added migration to add new coloumn to the booking table

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ const bodyParser = require('body-parser');
 const { PORT } = require('./config/configServer')
 const app = express();
 const apiRoutes = require('./routes/index')
-
+const db = require('./models/index')
 const setupAndStartServer = () => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended : true}));
     app.use('/api',apiRoutes);
     app.listen(PORT, () => {
+        // if(process.env.DB_SYNC) {
+        //     db.sequelize.sync({alter : true})
+        // }
         console.log(`Server is started at Port ${PORT}`);
     })
 }
